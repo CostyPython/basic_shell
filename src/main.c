541d59f5db57455bca409886c3aca065e873ad02
Harry Alcock <AlcockH@cardiff.ac.uk>
2026-07-08 15:47:17 +0100

    adding terminal flow and ability to clear terminal loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
-void main()
-{
-    printf("SHELL TERMINAL> ");
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
 
-    char input[100];
-    fgets(input, sizeof(input), stdin);
+void promptLoop(void);
+void identifyCommand(char *command);
+
+int main(void)
+{
+    promptLoop();
+    return 0;
+}
+
+void promptLoop(void)
+{
+
+    char command[256];
+    int index = 0;
+
+    printf("\nTERMINAL> ");
+
+    while (1)
+    {
+        if (_kbhit())
+        {
+            char key = _getch();
+            if (key == '\r')
+            {
+                identifyCommand(command);
+                promptLoop();
+            }
+            else
+            {
+                printf("%c", key);
+                command[index] = key;
+                index++;
+            }
+        }
+    }
+}
+
+void identifyCommand(char *command)
+{
+
+    if (strcmp(command, "clear") == 0)
+    {
+        system("cls");
+    }
 }
